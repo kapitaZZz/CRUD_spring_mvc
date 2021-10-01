3a650491bd6c33313cde3bbc046923027ffdd1b9
Anton Garussky <www.snowfall@mail.ru>
2021-10-01 13:31:48 +0300

    2.4.2 complete

---
+++ src/main/java/crud/service/RoleService.java
@@ -0,0 +1,19 @@
+package crud.service;
+
+import crud.model.Role;
+
+import java.util.List;
+
+public interface RoleService {
+
+    void addRole(Role role);
+
+    void updateRole(Role role);
+
+    void removeRoleById(long id);
+
+    List<Role> getAllRoles();
+
+    Role getRoleByName(String name);
+}
+
---
+++ src/main/java/crud/service/RoleServiceImpl.java
@@ -0,0 +1,51 @@
+package crud.service;
+
+import crud.dao.RoleDao;
+import crud.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+
+public class RoleServiceImpl implements RoleService {
+
+    private RoleDao roleDao;
+
+    @Autowired
+    public void setRoleDao(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    @Override
+    @Transactional
+    public void addRole(Role role) {
+        roleDao.addRole(role);
+    }
+
+    @Override
+    @Transactional
+    public void updateRole(Role role) {
+        roleDao.updateRole(role);
+    }
+
+    @Override
+    @Transactional
+    public void removeRoleById(long id) {
+        roleDao.removeRoleById(id);
+    }
+
+    @Override
+    @Transactional (readOnly = true)
+    public List<Role> getAllRoles() {
+        return roleDao.getAllRoles();
+    }
+
+    @Override
+    @Transactional (readOnly = true)
+    public Role getRoleByName(String name) {
+        return roleDao.getRoleByName(name);
+    }
+}
